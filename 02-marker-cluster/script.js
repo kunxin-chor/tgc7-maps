615512a1a3160d545a656d04efcdad6651ccca23
kunxin-chor <chorkunxin@yahoo.com>
2020-07-15 06:50:49 +0000

    Added cluster example

--- 02-marker-cluster/script.js
+++ 02-marker-cluster/script.js
@@ -1,3 +1,19 @@
+// the map argument refers to the map which we create using Leaflet
+function getRandomLatLng(map) {
+    // get the boundaries of the map
+    let bounds = map.getBounds();
+    let southWest = bounds.getSouthWest();
+    let northEast = bounds.getNorthEast();
+    let lngSpan = northEast.lng - southWest.lng;
+    let latSpan = northEast.lat - southWest.lat;
+
+    let randomLng = Math.random() * lngSpan + southWest.lng;
+    let randomLat = Math.random() * latSpan + southWest.lat;
+
+    return [ randomLat, randomLng,];
+}
+
+
 let singapore = [ 1.29,103.85]; // Singapore latlng
 let map = L.map('map').setView(singapore, 13);
 
@@ -10,3 +26,13 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw' //demo access token
 }).addTo(map);
+
+// create marker cluster
+let markerClusterLayer = L.markerClusterGroup();
+
+for (let i = 0; i < 1000; i++) {
+    let pos = getRandomLatLng(map);
+    L.marker(pos).addTo(markerClusterLayer);
+}
+
+markerClusterLayer.addTo(map);
